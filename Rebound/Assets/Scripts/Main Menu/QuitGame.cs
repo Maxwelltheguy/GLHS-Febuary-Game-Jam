31ce731cf4785c9ea363c41fbb9d9742fb055dd5
CSman16 <cadesmith538@gmail.com>
2026-02-16 16:46:20 -0500

    QuitButtonWorking?

---
+++ Rebound/Assets/Scripts/Main Menu/QuitGame.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor; 
+
+public class QuitGame : MonoBehaviour
+{
+    [SerializeField] private Button quitButton;
+
+    void Start()
+    {
+        // looks out for click
+        quitButton.onClick.AddListener(ExitGame);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quitting game now >:(");
+
+// if in the unity editor
+        EditorApplication.isPlaying = false;
+// if in build
+        Application.Quit();
+
+    }
+}
